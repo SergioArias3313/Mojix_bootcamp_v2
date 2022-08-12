7c18e4341b6b575982a766b0343feebb96ac8d95
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 21:07:16 -0400

    creating exercise 1 - tricks

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -2,7 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
+st.markdown('Simple but effective tips for every python lovers')
+
 st.markdown('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can urprise you with their amazing capabilities.')
+st.markdown('In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
+st.markdown(' . . .')
+
+st.header('1. Walrus operator')
+st.markdown('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
 
 st.subheader('Example')
 code_1 = ''' Mylist = [1,2,3]
@@ -13,3 +20,5 @@ st.code(code_1, language='python')
 
 st.subheader('Output')
 st.write("3")
+
+st.header('2. Splitting a string')
