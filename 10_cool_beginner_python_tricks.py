af3c93aeda6673548b8d06abb83d3e684adf63f1
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 21:39:49 -0400

    cool tricks

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -59,4 +59,17 @@ st.subheader('Output')
 code_3_2 = '''!dlrow olleh'''
 st.code(code_3_2, language='python')
 
-#--------------------------
+#------------- 4 -------------
+
+st.header('4. Merging two dictionaries')
+st.markdown('This amazing trick will help you merge two dictionaries with just 1 line of code. We just need to use ** in front of the name of the two dictionaries like below two merge them into a single dictionary:') 
+st.subheader('Example')
+code_3_1 = ''' d1 = {“a”: 10, “b”:20}
+    d2 = {“c”: 30, “d”:40}
+    d3 = {**d1, **d2}
+    print(d3)'''
+st.code(code_3_1, language='python')
+
+st.subheader('Output')
+code_3_2 = '''{‘a’: 10, ‘b’: 20, ‘c’: 30, ‘d’: 40}'''
+st.code(code_3_2, language='python')
