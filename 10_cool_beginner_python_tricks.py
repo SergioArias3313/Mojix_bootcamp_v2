88bd31ea796a98e962c63ee938489a8306384071
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 20:48:38 -0400

    cool tricks

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
-st.code("Mylist = [1,2,3]")
-#st.markdown(body, unsafe_allow_html=False)
 
-#Mylist = [1,2,3]
-#if(l := len(mylist) > 2): 
-#    print(l)
-#st.write("l")
+st.subheader('example')
+st.code("Mylist = [1,2,3]")
+st.code("if(l := len(mylist) > 2)")
+st.code( " print(l)")
+
+st.subheader('OUTPUT')
+st.write("3")
