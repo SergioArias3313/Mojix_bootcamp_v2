e9bc1c047774994ff330157933c816517d18ccba
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 20:35:53 -0400

    change 1

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Prueba")
+st.write("Let's star with the tricks")
+
+Mylist = [1,2,3]
+if(l := len(mylist) > 2): 
+    print(l)
+    st.write(l)
