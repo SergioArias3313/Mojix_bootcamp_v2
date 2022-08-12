271a032e0e62d7376e37f3a4c0e7306f671a23b4
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 20:52:17 -0400

    cool tricks

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
-st.markdown('The compactness of Python can make a developer’s life a lot easier when writing')
-st.markdown('lines and lines of code. But there are some lesser-known Python tricks that can')
-st.markdown('surprise you with their amazing capabilities.')
+st.markdown('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can urprise you with their amazing capabilities.')
 
 st.subheader('example')
 st.code("Mylist = [1,2,3]")
