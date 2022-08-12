252577137871c2824325230e52a01039820f4cbb
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 20:42:29 -0400

    change one

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
-st.markdown(body, unsafe_allow_html=False)
+st.code("Mylist = [1,2,3]")
+#st.markdown(body, unsafe_allow_html=False)
 
 #Mylist = [1,2,3]
 #if(l := len(mylist) > 2): 
 #    print(l)
-st.write("l")
+#st.write("l")
