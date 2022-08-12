37630d8f0b47eb4137fa784fcca1c0b0973291e6
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 21:00:52 -0400

    change one

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -5,10 +5,11 @@ st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 st.markdown('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can urprise you with their amazing capabilities.')
 
 st.subheader('Example')
-code_1 = '''  Mylist = [1,2,3]
-    if(l := len(mylist) > 2)
-    print(l) '''
-st.code(code_1, lenguage='python')
+code_1 = ''' Mylist = [1,2,3]
+    if(l := len(mylist) > 2):
+        print(l) '''
+
+st.code(code_1, language='python')
 
 st.subheader('Output')
 st.write("3")
