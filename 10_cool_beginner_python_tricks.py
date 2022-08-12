525a3ce968a4323da3d3b89866431a80a3cdf4c8
Sergio Arias <sergioarias3313@gmail.com>
2022-08-11 21:46:15 -0400

    cool tricks

--- 10_cool_beginner_python_tricks.py
+++ 10_cool_beginner_python_tricks.py
@@ -64,12 +64,48 @@ st.code(code_3_2, language='python')
 st.header('4. Merging two dictionaries')
 st.markdown('This amazing trick will help you merge two dictionaries with just 1 line of code. We just need to use ** in front of the name of the two dictionaries like below two merge them into a single dictionary:') 
 st.subheader('Example')
-code_3_1 = ''' d1 = {“a”: 10, “b”:20}
-    d2 = {“c”: 30, “d”:40}
-    d3 = {**d1, **d2}
-    print(d3)'''
-st.code(code_3_1, language='python')
+
+code_4_1 = ''' d1 = {“a”: 10, “b”:20}
+d2 = {“c”: 30, “d”:40}
+d3 = {**d1, **d2}
+print(d3)'''
+
+st.code(code_4_1, language='python')
 
 st.subheader('Output')
-code_3_2 = '''{‘a’: 10, ‘b’: 20, ‘c’: 30, ‘d’: 40}'''
-st.code(code_3_2, language='python')
+code_4_2 = '''{‘a’: 10, ‘b’: 20, ‘c’: 30, ‘d’: 40}'''
+st.code(code_4_2, language='python')
+
+#------------- 5 -----------------
+
+st.header('5. The zip() function')
+st.markdown('The zip() function in python can make your life a lot easier when working with lists and dictionaries. It is used to combine several lists of the same length.') 
+st.subheader('Example')
+
+code_5_1 = ''' colour = [“red”, “yellow”, “green”]
+fruits = [‘apple’, ‘banana’, ‘mango’]
+for colour, fruits in zip(colour, fruits):
+print(colour, fruits)'''
+
+st.code(code_5_1, language='python')
+
+st.subheader('Output')
+code_5_2 = '''red apple
+yellow banana
+green mango'''
+
+st.code(code_5_2, language='python')
+st.markdown('The zip() function can also be used for combining two lists into a dictionary. This method can be really helpful while grouping data from the list.')
+
+st.subheader('Example')
+code_5_3 = '''[“Rajesh”, “kumar”, “Kriti”]
+marks = [87, 90, 88]
+dictionary = dict(zip(students, marks))
+print(dictionary)'''
+
+st.code(code_5_3, language='python')
+
+st.subheader('Output')
+code_5_4 = '''{‘Rajesh’: 87, ‘kumar’: 90, ‘Kriti’: 88}'''
+
+st.code(code_5_4, language='python')
